a8520af3a420bf2ec3b4d8b543a8f6f37820dc38
Erik Steffl <erik@aintuinc.com>
2016-02-05 18:21:40 -0800

    listen and serve on port :8765

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
-// package github.com/zasran/HelloGo
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+
+	port := ":8765"
+
+	log.Printf("Serving on port [%s]", port)
+
+	log.Fatal(http.ListenAndServe(port, router))
 }
 
 // eof
